dee876649d9a71f9f946c8da722ac166e78886fb
laurac <laurac0362@gmail.com>
2024-12-16 16:40:20 +0100

    style container blue responsive colonnes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,48 +5,73 @@ import departementsData from '../components/data/departements.json';
 import Rating from '../components/Rating';
 
 const Home = () => {
-  // Filtrer les artisans ayant la propriété "top" à true
+  // Filtre des artisans ayant la propriété "top" à true dans datas.json
   const topArtisans = artisansData.filter((artisan) => artisan.top);
 
   return (
     <>
-    
+     <div style={{ backgroundColor: 'var(--color-secondary)' }} className="text-white pt-8">
+  <div className="max-w-lg mx-auto md:max-w-[850px] p-5">
+    <div className="md:flex md:items-center md:space-x-5">
+      <h1 className="text-2xl mb-6 md:mb-0 flex items-center justify-center md:text-center md:px-2 md:max-w-[350px]
+      ">
+        Comment trouver mon artisan ?
+      </h1>
+      <div className="space-y-5 text-left md:text-lg">
+        <p className="flex items-center">
+          <span
+            style={{ backgroundColor: 'var(--color-primary)' }}
+            className="text-white rounded-full w-8 h-8 flex items-center justify-center mr-3"
+          >
+            1
+          </span>
+          Choisir la catégorie d'artisanat dans le menu.
+        </p>
+        <p className="flex items-center">
+          <span
+            style={{ backgroundColor: 'var(--color-primary)' }}
+            className="text-white rounded-full w-8 h-8 flex items-center justify-center mr-3"
+          >
+            2
+          </span>
+          Choisir un artisan.
+        </p>
+        <p className="flex items-center">
+          <span
+            style={{ backgroundColor: 'var(--color-primary)' }}
+            className="text-white rounded-full w-8 h-8 flex items-center justify-center mr-3"
+          >
+            3
+          </span>
+          Le contacter via le formulaire de contact.
+        </p>
+        <p className="flex items-center">
+          <span
+            style={{ backgroundColor: 'var(--color-primary)' }}
+            className="text-white rounded-full w-8 h-8 flex items-center justify-center mr-3"
+          >
+            4
+          </span>
+          Une réponse sera apportée sous 48h.
+        </p>
+      </div>
+    </div>
+  </div>
 
 
-      <div style={{ backgroundColor: 'var(--color-secondary)'}} className=" text-white pt-8">
-        <div className="max-w-lg mx-auto text-center mx-auto max-w-[900px]">
-          <h1 className="text-xl  mb-6">Comment trouver mon artisan ?</h1>
-          <div className="space-y-4 text-left">
-            <p className="flex items-center">
-              <span style={{ backgroundColor: 'var(--color-primary)'}} className=" text-white rounded-full w-8 h-8 flex items-center justify-center mr-3 ">1</span>
-              Choisir la catégorie d'artisanat dans le menu.
-            </p>
-            <p className="flex items-center">
-              <span style={{ backgroundColor: 'var(--color-primary)'}} className=" text-white rounded-full w-8 h-8 flex items-center justify-center mr-3 ">2</span>
-              Choisir un artisan.
-            </p>
-            <p className="flex items-center">
-              <span style={{ backgroundColor: 'var(--color-primary)'}} className=" text-white rounded-full w-8 h-8 flex items-center justify-center mr-3 ">3</span>
-              Le contacter via le formulaire de contact.
-            </p>
-            <p className="flex items-center">
-              <span style={{ backgroundColor: 'var(--color-primary)'}} className=" text-white rounded-full w-8 h-8 flex items-center justify-center mr-3 ">4</span>
-              Une réponse sera apportée sous 48h.
-            </p>
-          </div>
-        </div>
+
         <div className="mt-8 text-center">
           <img src="/bulle-haut.png" alt="Bulle d'information" className="mx-auto" />
         </div>
       </div>
 
       <div className="mx-auto max-w-[900px]">
-        <div className="max-w-7xl mx-auto px-4">
-          <div className="mb-8">
-            <div className="flex items-center mb-2">
-              <VscDash className="text-7xl" style={{ color: 'var(--color-primary)'}} />
+        <div className="max-w-7xl mx-auto px-10">
+          <div className="mb-5">
+            <div>
+              <VscDash className="dash text-7xl" style={{ color: 'var(--color-primary)' }} />
             </div>
-            <h1 className="text-2xl" style={{ color: 'var(--color-secondary)'}}>Artisans du mois</h1>
+            <h1 className="text-xl" style={{ color: 'var(--color-secondary)' }}>Artisans du mois</h1>
           </div>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 text-center">
@@ -54,24 +79,58 @@ const Home = () => {
               const departement = departementsData[artisan.location];
 
               return (
-                <div key={artisan.id} style={{ backgroundColor: 'var(--color-white)' }} className="text-dark p-6 t">
-                  <h2 className="text-lg mb-2" style={{ color: 'var(--color-secondary)'}}>{artisan.name}</h2>
-                  <p className="text-sm mb-2">
+                <div
+                  key={artisan.id}
+                  style={{ backgroundColor: 'var(--color-white)' }}
+                  className="text-dark p-6 border border-transparent transition-all duration-300 group relative"
+                >
+                  <h2
+                    className="text-lg mb-2"
+                    style={{ color: 'var(--color-secondary)' }}
+                  >
+                    {artisan.name}
+                  </h2>
+                  <p className="mb-2">
                     <strong>{artisan.note}</strong> <Rating note={artisan.note} />
                   </p>
-                  <p className="text-sm mb-2">
+                  <p className="mb-2">
                     <strong>{artisan.specialty}</strong>. {artisan.about}
                   </p>
-                  <p className="text-sm">
-                    <strong>{artisan.location}</strong>{departement && ` (${departement})`}
+                  <p>
+                    <strong>{artisan.location}</strong>
+                    {departement && ` (${departement})`}
                   </p>
+
+                  {/* Icône flèche */}
+                  <span className="absolute bottom-4 left-1/2 transform -translate-x-1/2 transition-transform duration-300 group-hover:scale-125 group-hover:text-[var(--color-secondary)]">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                      />
+                    </svg>
+                  </span>
                 </div>
               );
             })}
           </div>
 
           <div className="my-8 text-center">
-          <button style={{ backgroundColor: 'var(--color-primary)' }} className=" text-white py-2 px-8 rounded-full transition-all duration-300 transform hover:scale-105" onMouseEnter={(e) => e.target.style.backgroundColor = 'var(--color-secondary)'} onMouseLeave={(e) => e.target.style.backgroundColor = 'var(--color-primary)'}>              Tous les artisans
+            <button
+              style={{ backgroundColor: 'var(--color-primary)' }}
+              className="text-white py-2 px-8 rounded-full transition-all duration-300 transform hover:scale-105"
+              onMouseEnter={(e) => e.target.style.backgroundColor = 'var(--color-secondary)'}
+              onMouseLeave={(e) => e.target.style.backgroundColor = 'var(--color-primary)'}
+            >
+              Tous les artisans
             </button>
           </div>
         </div>
