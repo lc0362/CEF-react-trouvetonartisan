4d723d9823892cfbb031596876d5842a2729752c
laurac <laurac0362@gmail.com>
2025-01-05 12:38:07 +0100

    base formulaire contact fiche

--- src/pages/Fiche.jsx
+++ src/pages/Fiche.jsx
@@ -4,6 +4,7 @@ import artisansData from '../components/data/datas.json';
 import departementsData from '../components/data/departements.json';
 import Rating from '../components/Rating';
 import { VscDash } from 'react-icons/vsc';
+import Breadcrumb from '../components/Breadcrumb.jsx';
 
 const Fiche = () => {
   const { name } = useParams(); // Récupérer le slug depuis l'URL
@@ -53,30 +54,79 @@ const Fiche = () => {
         <div className="max-w-7xl mx-auto px-10 lg:px-0">
           {/* Titre de la section */}
           <div className="mb-5 lg:pl-20">
+          <div className=" text-xs">
+            < Breadcrumb />
+            </div>
+            <h1 className="text-xl py-5" style={{ color: 'var(--color-secondary)' }}>
+              {artisan.name}
+            </h1>
+
+
             <div>
               <VscDash className="dash text-7xl" style={{ color: 'var(--color-primary)' }} />
             </div>
-            <h1 className="text-xl" style={{ color: 'var(--color-secondary)' }}>
-              {artisan.name}
-            </h1>
+            {/* A propos */}
+            <h2 className="text-xl" style={{ color: 'var(--color-secondary)' }}>
+             A propos
+            </h2>
           </div>
 
-          {/* Infos de l'artisan */}
-          <div className="text-lg mb-5">
+          
+          <div className="my-10 space-y-5">
             <div>
-              <strong>Note :</strong> {artisan.note} <Rating note={artisan.note} />
+            <strong>{artisan.note}</strong> <Rating note={artisan.note} />
             </div>
             <p>
-              <strong>Spécialité :</strong> {artisan.specialty}
+              <strong>Spécialité :</strong> {artisan.specialty}. {artisan.about}
             </p>
+           
+          
             <p>
               <strong>Localisation :</strong> {artisan.location}
-              {departement && ` (${departement})`}
-            </p>
-            <p>
-              <strong>À propos :</strong> {artisan.about}
+              {departement && ` (${departement})`}.
             </p>
           </div>
+
+          <div>
+              <VscDash className="dash text-7xl" style={{ color: 'var(--color-primary)' }} />
+            </div>
+            {/* Formulaire de contact */}
+            <h2 className="text-xl" style={{ color: 'var(--color-secondary)' }}>
+             Envoyer un mail à cet artisan
+            </h2>
+         
+          
+
+          
+          <div className="my-10 ml-10 space-y-5 max-w-[500px]">
+            <p className="text-xs italic ">Les champs marqués d'un astérisque (*) sont requis.</p>
+            <form className="space-y-5">
+            <label className="flex flex-col" >
+              <div className="flex flex-row pb-3">
+                Nom <div style={{ color: 'var(--color-accent)' }}>*</div>:
+              </div>
+                <input className="border rounded-full outline outline-1 outline-[var(--color-primary)] " 
+                style={{ backgroundColor: 'var(--color-white)' }} 
+                type="text" 
+                name="Nom" 
+                />
+
+              </label>
+
+              <p className="text-xs italic ">
+                Les informations recueillies à partir de ce formulaire 
+                sont nécessaires aux services de la Région Auvergne-Rhône-Alpes pour 
+                la gestion de votre demande 
+              </p>
+              <p className="text-xs italic"
+              style={{ color: 'var(--color-primary)' }}>
+                Pour en savoir plus sur la gestion de vos données et vos droits. 
+              </p>
+              <input className="border rounded-full hover:scale-110 hover:cursor-pointer px-6 py-1"
+              style={{ backgroundColor: 'var(--color-primary)', color: 'var(--color-white)' }}
+              type="submit" value="Soumettre" />
+              </form>
+              </div>
 
           {/* Bouton retour */}
           <div className="text-center mt-5">
